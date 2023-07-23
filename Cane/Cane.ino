fe800f451a0203ce52677e63525560ba19086d8a
RanShuai 2633609R <123078993+1RanShy@users.noreply.github.com>
2023-07-23 21:36:42 +0100

    cane code update

--- Cane/Cane.ino
+++ Cane/Cane.ino
@@ -1,36 +1,65 @@
 #include <SoftwareSerial.h>
 #include "hc_sr04.h"
 
-
-SoftwareSerial mySerial(4, 0); // RX, TX
-SR04 s1(4, 0);
+SoftwareSerial mySerial(2, 3); // RX, TX
+SR04 s1(13, 12); //echo trig
 float distance = 0.0;
-String test = "1345";
+String x = "xxx";
+float setDistance = 50;
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-
-
-
-  Serial.println("Goodnight moon!");
-
-  // set the data rate for the SoftwareSerial port
   mySerial.begin(9600);
-  mySerial.println("Hello, world?");
+
 }
 
 void loop() { // run over and over
-  
+
   distance = s1.getDistance();
-  
   if (mySerial.available()) {
-    mySerial.println("Received");
-    test = mySerial.readString();
+
+    x = mySerial.readString();
+    if (x == "20")
+    {
+      setDistance = 20;
+    }
+    if (x == "40")
+    {
+      setDistance = 40;
+    }
+
+    if (x == "60")
+    {
+      setDistance = 60;
+    }
+    if (x == "80")
+    {
+      setDistance = 80;
+    }
+
+    if (x == "100")
+    {
+      setDistance = 100;
+    }
+    if (x == "150")
+    {
+      setDistance = 150;
+    }
+    if (x == "200")
+    {
+      setDistance = 200;
+    }
   }
-  mySerial.println("12345");
-  Serial.println(test);
-  Serial.println("Goodnight moon!");
-//  Serial.println("diatance");
+  if (distance <= setDistance)
+  {
+    mySerial.println("Unsafe");
+  }
+  else
+  {
+    mySerial.println("Safe");
+  }
+  mySerial.println(distance);
+  Serial.println(x);
   delay(500);
 
 }
